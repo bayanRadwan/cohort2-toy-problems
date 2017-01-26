e0455d829206acc2b7035cfd700703abafc8a9f6
bayan radwan <bayanradwan@bayans-MacBook-Air.local>
2017-01-26 09:10:19 +0200

    binary

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,8 +9,21 @@
  * 
  * console.log(index); // 4
  */
-
-var binarySearch = function (array, target) {
-  
+// [1,2,3,4,5,6,7]
+var binarySearch = function (array, target) { // target = 6
+  var min = 0; //min = 0
+  var max = array.length-1; // max = 7 
+  var midd , elem;
+  while(min <= max){
+  	midd= Math.floor((min + max) /2 , 10) // midd = 3
+  	elem = array[midd]; // elem = 4
+  	if(elem < target){
+  		min  = midd +1; // min = 4
+  	}else if(elem > target){ 
+  		max = midd -1;
+  	}else {
+  		return midd;
+  	}
+  }
+  return "target not found";
 };
-
